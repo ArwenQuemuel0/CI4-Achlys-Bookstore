14d419d7557aa9a8566378612605f4b954846168
ArwenQuemuel0 <arwen.quemuel.030@gmail.com>
2025-11-02 22:37:27 +0800

    refactor(database/users): replaced migration for users table

--- backend/app/Database/Seeds/DatabaseSeeder.php
+++ backend/app/Database/Seeds/DatabaseSeeder.php
@@ -9,7 +9,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call('App\\Database\\Seeds\\ClearDatabaseSeeder');
-
-        // $this->call('App\\Database\\Seeds\\<name of the seeder here>');
     }
 }
