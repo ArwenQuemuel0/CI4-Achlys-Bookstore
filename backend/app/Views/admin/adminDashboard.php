a10c8feb5df584c74c2e4e5b5a28dccfffa13e43
ArwenQuemuel0 <arwen.quemuel.030@gmail.com>
2025-11-03 00:59:34 +0800

    feat(frontend/adminDashboard): added icon

--- backend/app/Views/admin/adminDashboard.php
+++ backend/app/Views/admin/adminDashboard.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard | Achlys' Bookstore</title>
+    <link rel="shortcut icon" type="image/png" href="/assets/bookstore_icon.ico" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
     <style>
